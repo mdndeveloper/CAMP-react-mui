dbd698c43a832bd0b4fb4227e86b51f3ca7a7877
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-05 18:11:11 +0600

    stream page design done

---
+++ src/pages/confugure/Streams.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ConfigureLayout from '../../components/configure/ConfigureLayout';
+import StreamContainer from '../../components/configure/streams/Stream';
+const Streams = () => {
+  return (
+    <ConfigureLayout>
+      <StreamContainer />
+    </ConfigureLayout>
+  );
+};
+
+export default Streams;
