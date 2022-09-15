591921cbc889ac5150ecd1d524c571ba72289e17
madan-kanala <mdnkanala.dev@gmail.com>
2022-09-15 21:36:35 +0600

    scheduled messages and admin default messages show in messages page

--- src/components/admin/defaultMessges/DefaultMessgaes.js
+++ src/components/admin/defaultMessges/DefaultMessgaes.js
@@ -1,10 +1,16 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMessagesAsync } from '../../../features/message/thunks';
 import AdminLayout from '../AdminLayout';
 import Create from './Create';
 import Library from './Library';
 
 const DefaultMessagesContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMessagesAsync());
+  }, [dispatch]);
   return (
     <AdminLayout>
       <Box
