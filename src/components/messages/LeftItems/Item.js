8c4d908affc15e7af70a673cc5954b3d28d66917
madan-kanala <mdnkanala.dev@gmail.com>
2022-10-23 20:59:36 +0600

    final bug fixing

--- src/components/messages/LeftItems/Item.js
+++ src/components/messages/LeftItems/Item.js
@@ -5,8 +5,12 @@ import moment from 'moment';
 import React, { useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessageAsync } from '../../../features/message/thunks';
+import { useGetConfigQuery } from '../../../features/userConfig/userConfigApiSlice';
+import { getAuthUserId } from '../../../utils/auth';
 
-const Item = ({ type, text, buttons }) => {
+const Item = ({ type, text, buttons, appendText }) => {
+  const { data: config, isSuccess } = useGetConfigQuery(getAuthUserId());
+
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
   const { isLoading } = useSelector((state) => state.messages);
@@ -24,20 +28,23 @@ const Item = ({ type, text, buttons }) => {
     setLoading(true);
     const data = {
       userId,
-      message,
+      message: message + ' ' + appendText,
       dateTime: moment(),
       days: 0,
-      duration: 0,
+      duration: config[0].defaultDuration,
       lastSent: null,
       type: '',
       category: type,
     };
+
     dispatch(addMessageAsync(data));
     setTimeout(() => {
       setLoading(isLoading);
       setMessage('');
     }, 2000);
   };
+
+  if (!isSuccess || data.length <= 0) return null;
   return (
     <>
       <Stack direction='row' gap={2} alignItems={'center'} flexWrap='wrap'>
--- src/components/messages/LeftItems/LeftItems.js
+++ src/components/messages/LeftItems/LeftItems.js
@@ -1,10 +1,15 @@
 import { Box } from '@mui/material';
 import { Stack } from '@mui/system';
 import React from 'react';
+import { useGetConfigQuery } from '../../../features/userConfig/userConfigApiSlice';
+import { getAuthUserId } from '../../../utils/auth';
 import Item from './Item';
 import QuickSendArea from './QuickSendArea';
 
 const LeftItems = () => {
+  const { data, isSuccess } = useGetConfigQuery(getAuthUserId());
+
+  if (!isSuccess || data.length <= 0) return null;
   return (
     <Box sx={{ boxSizing: 'border-box', px: '10px' }}>
       <Stack gap={4}>
@@ -12,11 +17,13 @@ const LeftItems = () => {
           type='checkin'
           text={'Check IN'}
           buttons={{ variant: 'contained', sx: { background: '#82da73' } }}
+          appendText={data[0].checkInMessage}
         />
         <Item
           type='checkout'
           text={'Check Out'}
           buttons={{ variant: 'contained', sx: { background: '#f46969' } }}
+          appendText={data[0].checkOutMessage}
         />
 
         <QuickSendArea />
